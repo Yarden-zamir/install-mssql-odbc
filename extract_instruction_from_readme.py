2a932af32adf8c422575e71af5389ee16b7afca7
Yarden Zamir <dev@yarden-zamir.com>
2023-05-09 16:08:39 +0300

    Add docstring

--- extract_instruction_from_readme.py
+++ extract_instruction_from_readme.py
@@ -1,15 +1,17 @@
+"""
+A script that scrapes the instructions from the microsoft odbc driver for linux readme file.
+"""
 from os import getenv
 
 from requests import get
 
 remove_exits = getenv('REMOVE_EXITS', "True")
-docs_url = getenv('DOCS_URL',
-                  "https://raw.githubusercontent.com/MicrosoftDocs/sql-docs/live/docs/connect/odbc/linux-mac"
-                  "/installing-the-microsoft-odbc-driver-for-sql-server.md")
+docs_url = getenv('DOCS_URL')
 distro = getenv("DISTRO", "Ubuntu")
 ODBC_version = getenv("ODBC_VERSION", "18")
 
-ODBC_section = get(docs_url).text.split(f"Microsoft ODBC {ODBC_version}")[1].split("---")[0]
+ODBC_section = get(docs_url, timeout=10).text.split(f"Microsoft ODBC {ODBC_version}")[1].split(
+    "---")[0]
 platforms = ODBC_section.split("### [")[1:]
 per_platform_instructions = {}
 for platform in platforms:
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
+[![Pylint](https://github.com/Yarden-zamir/install-mssql-odbc/actions/workflows/pylint.yml/badge.svg)](https://github.com/Yarden-zamir/install-mssql-odbc/actions/workflows/pylint.yml)
 # install-mssql-odbc
 Action for installing ODBC drivers for mssql exactly according to [Microsoft's instructions](https://learn.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-ver16&tabs=redhat18-install%2Calpine17-install%2Cdebian8-install%2Credhat7-13-install%2Crhel7-offline#18).
 
