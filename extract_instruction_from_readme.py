b561b55b35cdcb274d118241127ea1f89ff2879e
Yarden Zamir <dev@yarden-zamir.com>
2023-05-09 14:34:03 +0300

    Use script to scrape instructions from docs

---
+++ extract_instruction_from_readme.py
@@ -0,0 +1,20 @@
+from os import getenv
+
+from requests import get
+
+remove_exits = getenv('REMOVE_EXITS', True)
+docs_url = getenv('DOCS_URL',
+                  "https://raw.githubusercontent.com/MicrosoftDocs/sql-docs/live/docs/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server.md")
+distro = getenv("DISTRO", "Ubuntu")
+
+ODBC_18_section = get(docs_url).text.split("Microsoft ODBC 18")[1].split("---")[0]
+
+platforms = ODBC_18_section.split("### [")[1:]
+
+per_platform_instructions = {}
+for platform in platforms:
+    name = platform.split("]")[0]
+    code = platform.split("```bash")[1].split("```")[0]
+    per_platform_instructions[name] = code.replace("exit\n", "") if remove_exits else code
+
+print(per_platform_instructions[distro])
